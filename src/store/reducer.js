833b3ba2cb57605e62120dfa39df90248f6d4bfe
myname-abhik <abhik16chakrabortty@gmail.com>
2024-05-14 02:58:19 +0530

    1

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -18,10 +18,13 @@ const stunServers = {
     //         // "stun:stun4.l.google.com:19302",
     //         // "stun:stun.services.mozilla.com",
     // ]
+    turn:'turn01.hubl.in?transport=udp', 
     
-        url: 'turn:numb.viagenie.ca',
-        credential: 'muazkh',
-        username: 'webrtc@live.com'
+        url: 'turn:192.158.29.39:3478?transport=udp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+    
+        
     
     } ]
 }   
